a55bbf09610ac548bdc21dc67daa528a351a7484
BheemaNaveen <b.naveen@altavistainfosolutions.in>
2024-06-11 21:27:21 +0530

    second commit

--- src/main/java/com/msmavas/speedy/models/User.java
+++ src/main/java/com/msmavas/speedy/models/User.java
@@ -2,14 +2,21 @@ package com.msmavas.speedy.models;
 
 import java.sql.Date;
 
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 //user
 @Getter
 @Setter
-@NoArgsConstructor
-@AllArgsConstructor
+
 @Entity
 @Table(name="user")
 public class User {
